814d912f0506f4aa6c956a093d214f8981830bfd
Matias-S1lva <matiasleandro752@gmail.com>
2022-10-02 13:08:24 -0300

    [FIX]carrito de  compras

--- react-practico-react-challenge/src/routes/App.jsx
+++ react-practico-react-challenge/src/routes/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
 	const initialState = useInitialState();
 	return (
 		<AppContext.Provider value={initialState}>
-		<BrowserRouter >
+		<BrowserRouter basename='/react-shop' >
 			<Layout>
 				<Switch>
 					<Route exact path="/" component={Home} />
